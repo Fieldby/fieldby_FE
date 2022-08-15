7af5c7c9fb0c058eeb56f6caea5784027e9afb4e
Shawn <63200799+TechD-lab@users.noreply.github.com>
2022-08-15 19:25:14 +0900

    로그인 버튼 hover 디자인

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,263 +1,295 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components"
+import styled from "styled-components";
 import { authService } from "../../fBase";
 
-
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    // const onChange = (event) => {
-    //     const {target : {name, value}} = event;
-    //     if (name === "email") {
-    //         setEmail(value);
-    //     } else if (name === "password") {
-    //         setPassword(value);
-    //     }
-    // }
+  // const onChange = (event) => {
+  //     const {target : {name, value}} = event;
+  //     if (name === "email") {
+  //         setEmail(value);
+  //     } else if (name === "password") {
+  //         setPassword(value);
+  //     }
+  // }
 
-    
-    const signIn = async () => {
-        try {
-            const user = await signInWithEmailAndPassword(authService, email,password);
-            console.log(user);
-            navigate("/campaign");
-        } catch (error) {
-            console.log(error.message);
-            alert("비밀번호를 확인해주세요.");
-        }
-    };
-
-    const enterLogin = () => {
-        if (window.event.keyCode === 13) {
-            signIn();
-        }
+  const signIn = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        authService,
+        email,
+        password
+      );
+      console.log(user);
+      navigate("/campaign");
+    } catch (error) {
+      console.log(error.message);
+      alert("비밀번호를 확인해주세요.");
     }
+  };
 
-    return( 
-        <LoginContainerCSS>
-            <div className="login-square-hc login-square-vc" id="login-cm">
-                <div className="login-logo">
-                    <img src="/images/fieldbylogo.png" alt=""/>
-                    <h2 className="login-logo-name">Business Suite</h2>
-                </div>
-                <div className="login-box-container">
-                    <div className="login-box">
-                        <input name="email" type="email" placeholder="이메일" className="login-email" value={email} onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}/>
-                        <input name="password" type="password" placeholder="비밀번호" className="login-pw" value={password} onKeyUp={enterLogin} onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}/>
-                    </div>
-                    {/* <div className="login-maintain">
+  const enterLogin = () => {
+    if (window.event.keyCode === 13) {
+      signIn();
+    }
+  };
+
+  return (
+    <LoginContainerCSS>
+      <div className="login-square-hc login-square-vc" id="login-cm">
+        <div className="login-logo">
+          <img src="/images/fieldbylogo.png" alt="" />
+          <h2 className="login-logo-name">Business Suite</h2>
+        </div>
+        <div className="login-box-container">
+          <div className="login-box">
+            <input
+              name="email"
+              type="email"
+              placeholder="이메일"
+              className="login-email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="비밀번호"
+              className="login-pw"
+              value={password}
+              onKeyUp={enterLogin}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          {/* <div className="login-maintain">
                         <img src="images/login-maintain.png" alt=""/>
                         <span className="login-maintain-check">로그인 유지</span>
                     </div> */}
-                    <div className="login-btn-wrapper">
-                        <button onClick={signIn} id="login-btn">로그인</button>
-                        
-                    </div>
-                </div>
-                
-                <div className="login-find-container">
-                    <div className="login-find-wrapper">
-                        <a href="/sign-up" className="login-signup">회원가입</a>
-                        <span>|</span>
-                        <a href="/find-pw" className="login-find-pw">비밀번호 찾기</a>
-                    </div>
-                </div>
-                <div className="login-info-container">
-                    <div className="login-info-wrapper">
-                        <a href="/privacy" className="login-privacy">개인정보처리방침</a>
-                        <span>|</span>
-                        <a href="/policy" className="login-policy">이용약관</a>
-                    </div>
-                    <span className="login-info-rights">ⓒ 2022 FIELDBY Corporation. All Rights Reserved.</span>
-                </div>
-            </div>
-        </LoginContainerCSS>   
-    )
-    
-}
+          <div className="login-btn-wrapper">
+            <button onClick={signIn} id="login-btn">
+              로그인
+            </button>
+          </div>
+        </div>
+
+        <div className="login-find-container">
+          <div className="login-find-wrapper">
+            <a href="/sign-up" className="login-signup">
+              회원가입
+            </a>
+            <span>|</span>
+            <a href="/find-pw" className="login-find-pw">
+              비밀번호 찾기
+            </a>
+          </div>
+        </div>
+        <div className="login-info-container">
+          <div className="login-info-wrapper">
+            <a href="/privacy" className="login-privacy">
+              개인정보처리방침
+            </a>
+            <span>|</span>
+            <a href="/policy" className="login-policy">
+              이용약관
+            </a>
+          </div>
+          <span className="login-info-rights">
+            ⓒ 2022 FIELDBY Corporation. All Rights Reserved.
+          </span>
+        </div>
+      </div>
+    </LoginContainerCSS>
+  );
+};
 
 const LoginContainerCSS = styled.div`
-    #login-cm {
-        position : absolute;
-        min-width : 672px;
-        min-height : 703px;
+  #login-cm {
+    position: absolute;
+    min-width: 672px;
+    min-height: 703px;
+  }
+  .login-square-hc {
+    width: 30%;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    background: #ffffff;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.07);
+    border-radius: 5px;
+  }
+  .login-square-vc {
+    height: 70%;
+    top: 0;
+    bottom: 0;
+    margin-bottom: auto;
+    margin-top: auto;
+  }
+  .login-logo {
+    margin-top: 20px;
+    margin-left: 16px;
+    img {
+      height: 43px;
+      width: 133px;
     }
-    .login-square-hc {
-        width : 30%;
-        left : 0;
-        right : 0;
-        margin-left : auto;
-        margin-right : auto;
-        background: #FFFFFF;
-        box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.07);
-        border-radius: 5px;  
+    .login-logo-name {
+      margin-left: 8px;
+      margin-block-start: -10px;
+      margin-block-end: 0;
+      font-size: 18px;
+      color: #303030;
     }
-    .login-square-vc {
-        height : 70%;
-        top : 0;
-        bottom : 0;
-        margin-bottom : auto;
-        margin-top : auto;
+  }
+
+  .login-box-container {
+    margin-top: 120px;
+    display: grid;
+
+    .login-box {
+      display: grid;
+      width: 100%;
+      .login-email {
+        height: 24px;
+        width: 45%;
+        left: 0;
+        rigth: 0;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 8px 10px;
+        background: #f1f1f1;
+        border: 1px solid #22baa8;
+        border-radius: 13px;
+        font-size: 11px;
+      }
+      .login-pw {
+        height: 24px;
+        width: 45%;
+        left: 0;
+        right: 0;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 8px 10px;
+        margin-top: 18px;
+        background: #f1f1f1;
+        border: 1px solid #22baa8;
+        border-radius: 13px;
+        font-size: 11px;
+      }
     }
-    .login-logo {
-        margin-top : 20px;
-        margin-left : 16px;
-        img {
-            height : 43px;
-            width : 133px;
-        }
-        .login-logo-name {
-            margin-left : 8px;
-            margin-block-start : -10px;
-            margin-block-end : 0;
-            font-size : 18px;
-            color : #303030;
-        }
+  }
+
+  .login-maintain {
+    margin-top: 110px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      height: 25px;
+      width: 25px;
+    }
+    .login-maintain-check {
+      font-style: normal;
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 19px;
+      margin-left: 8px;
+    }
+  }
+
+  .login-btn-wrapper {
+    display: flex;
+    justify-content: center;
+    margin-top: 110px;
+
+    #login-btn {
+      background: #303030;
+      border-radius: 5px;
+      border: none;
+      width: 35%;
+      height: 46px;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 19px;
+      text-align: center;
+      color: #ffffff;
+      cursor: pointer;
+      transition: all 0.5s;
     }
 
-    .login-box-container {
-        margin-top : 120px;
-        display : grid;
-
-        .login-box {
-            display : grid;
-            width : 100%;
-            .login-email {
-                height : 24px;
-                width : 45%;
-                left : 0;
-                rigth : 0;
-                margin-left : auto;
-                margin-right : auto;
-                padding : 8px 10px;
-                background: #F1F1F1;
-                border: 1px solid #22BAA8;
-                border-radius: 13px;
-                font-size : 11px;
-            }
-            .login-pw {
-                height : 24px;
-                width : 45%;
-                left : 0;
-                right : 0;
-                margin-left : auto;
-                margin-right : auto;
-                padding : 8px 10px;
-                margin-top : 18px;
-                background: #F1F1F1;
-                border: 1px solid #22BAA8;
-                border-radius: 13px;
-                font-size : 11px;
-            }
-        }
+    #login-btn:hover {
+      background: #22baa8;
     }
+  }
 
-    .login-maintain {
-        margin-top : 110px;
-        display : flex;
-        justify-content : center;
-        align-items : center;
-        img {
-            height : 25px;
-            width : 25px;
-        }
-        .login-maintain-check {
-            font-style: normal;
-            font-weight: 700;
-            font-size: 16px;
-            line-height: 19px;
-            margin-left : 8px;
-        }
+  .login-find-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 80px;
+    .login-find-wrapper {
+      display: flex;
+      justify-content: space-between;
+      .login-signup {
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+        margin-right: 6px;
+        color: #303030;
+        text-decoration: none;
+      }
+      .login-find-pw {
+        margin-left: 6px;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+        color: #303030;
+        text-decoration: none;
+      }
     }
-    
-    .login-btn-wrapper {
-        display : flex;
-        justify-content : center;
-        margin-top : 110px;
+  }
 
-        #login-btn{
-            background: #303030;
-            border-radius: 5px; 
-            border : none;
-            width : 35%;
-            height : 46px;
-            font-style: normal;
-            font-weight: 700;
-            font-size: 16px;
-            line-height: 19px;
-            text-align: center;
-            color: #FFFFFF;
-        }
+  .login-info-container {
+    display: grid;
+    justify-content: center;
+    text-align: center;
+    margin-top: 50px;
+    margin-bottom: 10px;
+    .login-info-wrapper {
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 17px;
+      .login-privacy {
+        margin-right: 3px;
+        text-decoration: none;
+        color: #8e8e8e;
+      }
+      .login-policy {
+        margin-left: 3px;
+        text-decoration: none;
+        color: #8e8e8e;
+      }
     }
 
-    .login-find-container {
-        display : flex;
-        justify-content : center;
-        margin-top : 80px;
-        .login-find-wrapper{
-            display : flex;
-            justify-content : space-between;
-            .login-signup {
-                font-style: normal;
-                font-weight: 700;
-                font-size: 16px;
-                line-height: 19px;
-                margin-right : 6px;
-                color : #303030;
-                text-decoration : none;
-            }
-            .login-find-pw {
-                margin-left : 6px;
-                font-style: normal;
-                font-weight: 700;
-                font-size: 16px;
-                line-height: 19px;
-                color : #303030;
-                text-decoration : none;
-            }
-        }
-        
+    .login-info-rights {
+      color: #8e8e8e;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 17px;
     }
-    
-    .login-info-container {
-        display : grid;
-        justify-content : center;
-        text-align : center;
-        margin-top : 50px;
-        margin-bottom : 10px;
-        .login-info-wrapper {
-            font-style: normal;
-            font-weight: 400;
-            font-size: 12px;
-            line-height: 17px;
-            .login-privacy {
-                margin-right : 3px;
-                text-decoration : none;
-                color : #8e8e8e;
-            }
-            .login-policy {
-                margin-left : 3px;
-                text-decoration : none;
-                color : #8e8e8e;
-            }
-        }
-        
-        .login-info-rights {
-            color : #8e8e8e;
-            font-style: normal;
-            font-weight: 400;
-            font-size: 12px;
-            line-height: 17px;
-        }
-    }
-`
+  }
+`;
 
 export default Login;
--- src/fBase.js
+++ src/fBase.js
@@ -1,22 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-import { getStorage } from 'firebase/storage'
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL:process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyD6-FoXJgmC1fkaeGkhvCaPkyeJPOtLTio",
+  authDomain: "fieldby-web.firebaseapp.com",
+  databaseURL:
+    "https://fieldby-web-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fieldby-web",
+  storageBucket: "fieldby-web.appspot.com",
+  messagingSenderId: "513070035696",
+  appId: "1:513070035696:web:213b83777dc425df830835",
+  measurementId: "G-MGB1RD8ZEF",
 };
 
 // Initialize Firebase
