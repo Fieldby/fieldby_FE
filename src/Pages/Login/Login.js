bc5595a77eef7d448884f2ee8f22577b783ab61c
Shawn <63200799+TechD-lab@users.noreply.github.com>
2022-08-15 19:33:47 +0900

    슬랙연동테스트

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -223,7 +223,7 @@ const LoginContainerCSS = styled.div`
       text-align: center;
       color: #ffffff;
       cursor: pointer;
-      transition: all 0.4s;
+      transition: all 0.3s;
     }
 
     #login-btn:hover {
